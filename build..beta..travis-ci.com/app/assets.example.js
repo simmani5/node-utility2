30a5702c6c661fda3ba5d88633fe57f9803d7e36
Travis CI User <travis@example.org>
2020-08-20 00:24:42 +0000

    [ci skip] update gh-pages

--- build..beta..travis-ci.com/app/assets.example.js
+++ build..beta..travis-ci.com/app/assets.example.js
@@ -101,8 +101,6 @@
 
 
 
-
-
 /*
 example.js
 
@@ -129,12 +127,12 @@ instruction
 // run shared js-env code - init-local
 (function () {
     "use strict";
-    let consoleError;
     let isBrowser;
     let isWebWorker;
     let local;
     // init debugInline
     if (!globalThis.debugInline) {
+        let consoleError;
         consoleError = console.error;
         globalThis.debugInline = function (...argList) {
         /*
@@ -162,8 +160,7 @@ instruction
     /*
      * this function will assert JSON.stringify(<aa>) === JSON.stringify(<bb>)
      */
-        let objectDeepCopyWithKeysSorted;
-        objectDeepCopyWithKeysSorted = function (obj) {
+        function objectDeepCopyWithKeysSorted(obj) {
         /*
          * this function will recursively deep-copy <obj> with keys sorted
          */
@@ -181,7 +178,7 @@ instruction
                 sorted[key] = objectDeepCopyWithKeysSorted(obj[key]);
             });
             return sorted;
-        };
+        }
         aa = JSON.stringify(objectDeepCopyWithKeysSorted(aa));
         bb = JSON.stringify(objectDeepCopyWithKeysSorted(bb));
         if (aa !== bb) {
